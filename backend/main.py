a264b0984eec86411b9b93e11249d1c2a4f2d27f
akshara <nukalaakshara21@gmail.com>
2026-09-18 00:08:42 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Student(BaseModel):
